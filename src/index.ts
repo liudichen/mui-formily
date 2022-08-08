425424ee6a057e59bd9c9da271e2866c250b631e
iimm <liudichen@foxmail.com>
2022-08-08 17:42:38 +0800

    >0.2.30Select\submit\reset\modalform改为从formfield引入

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,10 @@
  * @Author: 柳涤尘 https://www.iimm.ink
  * @LastEditors: 柳涤尘 liudichen@foxmail.com
  * @Date: 2022-04-11 22:17:08
- * @LastEditTime: 2022-08-05 22:02:44
+ * @LastEditTime: 2022-08-08 17:25:19
  */
-import KeyWords from './KeyWords';
+import { FormilyModalForm, FormilyModalFormProps, FormilyReset, FormilyResetProps, FormilySelect, FormilySelectProps, FormilySubmit, FormilySubmitProps } from 'mui-formfield';
 export { default as Input, InputProps } from './Input';
-export { default as Select, SelectProps } from './Select';
 export { default as Switch, SwitchProps } from './Switch';
 export { default as DatePicker, DatePickerProps } from './DatePicker';
 export { default as DateRangePicker, DateRangePickerProps } from './DateRangePicker';
@@ -16,13 +15,18 @@ export { default as RadioGroup, RadioGroupProps } from './RadioGroup';
 export { default as Upload, UploadProps } from './Upload';
 export { default as Transfer, TransferProps } from './Transfer';
 export { default as EditableTable, EditableTableProps } from './EditableTable';
-export { KeyWordsProps } from './KeyWords';
-export { KeyWords };
+export { default as KeyWords, KeyWordsProps } from './KeyWords';
 
+export {
+  FormilyModalForm as ModalForm,
+  FormilyModalFormProps as ModalFormProps,
+  FormilyReset as Reset,
+  FormilyResetProps as ResetProps,
+  FormilySelect as Select,
+  FormilySelectProps as SelectProps,
+  FormilySubmit as Submit,
+  FormilySubmitProps as SubmitProps,
+};
 
-export { default as Reset, ResetProps } from './Reset';
-export { default as Submit, SubmitProps } from './Submit';
-
-export { default as ModalForm, ModalFormProps } from './ModalForm';
 export { default as StepsForm, StepsFormProps, StepFormProps, ResultRenderProps, StepIconProps } from './StepsForm';
 
